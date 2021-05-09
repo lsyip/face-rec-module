f231194f34b16d858b413faae8808084cc882246
Lynn Yip <50850731+lsyip@users.noreply.github.com>
2021-05-09 12:45:59 -0400

    Added function descriptions

--- face-rec-module.js
+++ face-rec-module.js
@@ -6,6 +6,7 @@
 */
 
 
+// Registers module with default values. 
 Module.register("face-rec-module", {
 	// anything here in defaults will be added to the config data
 	// and replaced if the same thing is provided in config
@@ -13,33 +14,37 @@ Module.register("face-rec-module", {
 		userName: "Unknown",
 	},
 
-	// socket notification received handler
+	// Handles received notifications from node_helper.js
 	socketNotificationReceived: function(notification, payload) {
+		// If a name is received from node_helper.js, update userName and mirror display.
 		if (notification === "NAME") {
 			console.log("Name notification received");
-			this.userName = payload;
+			this.userName = payload;   // payload represents the name returned from RecognizeUser.py
 			console.log("New name written " + payload);
-			this.updateDom();
+			this.updateDom();     // Updates mirror display
 			}
+		// Else log the event to the console
 		else {
 			console.log("Name notification NOT received");
 			}
 		},
 		
+	// Initiates first request to node_helper to retrieve user's name.
 	start: function(){
 		console.log(this.name + " is starting!");
-		this.sendSocketNotification("GET NAME", "initial");
+		this.sendSocketNotification("GET NAME", "initial");   // Send "GET NAME" notification to node_helper
 
 	},
 
 
 	// only called if the module header was configured in module config in config.js
+	// Displays title on top of module. 
 	getHeader: function() {
 		return "Hello, CIS111B";
 	},
 
 
-	// this is the major worker of the module, it provides the displayable content for this module
+	// Displays HTML content on the mirror. 
 	getDom: function() {
 		var wrapper = document.createElement("div");
 
